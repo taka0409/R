ee0eecbdb8a678e0ebfbb716e7b3b50383356a79
taka0409 <32897506+taka0409@users.noreply.github.com>
2017-12-05 17:44:39 +0900

    Update 4.R

--- 4.R
+++ 4.R
@@ -5,8 +5,16 @@ tmp_time <- paste(paste(df$year, df$month, df$day, sep="-"),
 tmp_time <- as.POSIXct(tmp_time)
 df$timestamp <- tmp_time
 df <- df[,c(1,2,6:14)]
-x <- df$TEMP
-y <- df$PRES
-deviation <- function(x) sqrt(var(x)*(length(x)-1)/length(x))
-cov(x,y)/sqrt(deviation(x))/sqrt(deviation(y))
-cor(df$TEMP,df$PRES)
+x<-df$TEMP
+y<-df$PRES
+Sx<-0
+Sy<-0
+Sxy<-0
+for(i in 1:length(x)){
+	Sxy<-Sxy+(x[i]-mean(x))*(y[i]-mean(y))
+	Sx<-Sx+(x[i]-mean(x))^2/length(x)
+	Sy<-Sy+(y[i]-mean(y))^2/length(y)
+}
+Sxy<-Sxy/length(x)
+Sxy/sqrt(Sx)/sqrt(Sy)
+cor(x,y)
