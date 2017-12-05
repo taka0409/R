2963de1a60626dee90089dff185d257e33129c2e
taka0409 <32897506+taka0409@users.noreply.github.com>
2017-12-05 18:12:16 +0900

    Create 5.R

---
+++ 5.R
@@ -0,0 +1,22 @@
+df <- read.table("data.csv",header = 1,sep = ",")
+df <- df[complete.cases(df),]
+tmp_time <- paste(paste(df$year, df$month, df$day, sep="-"),  
+              paste(df$hour, 00, 00, sep=":"), sep = " ") 
+tmp_time <- as.POSIXct(tmp_time)
+df$timestamp <- tmp_time
+df <- df[,c(1,2,6:14)]
+x<-df$TEMP
+y<-df$PRES
+Sx<-0
+Sy<-0
+Sxy<-0
+for(i in 1:length(x)){
+	Sxy<-Sxy+(x[i]-mean(x))*(y[i]-mean(y))
+	Sx<-Sx+(x[i]-mean(x))^2/length(x)
+	Sy<-Sy+(y[i]-mean(y))^2/length(y)
+}
+Sxy<-Sxy/length(x)
+Sxy/Sx
+df_lm<-lm(PRES ~ TEMP,data=df)
+plot(x,y)
+abline(df_lm,col="red")
