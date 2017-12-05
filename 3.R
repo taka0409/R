9e763997362375ac5ae0f1379f3e42fca8a54f87
taka0409 <32897506+taka0409@users.noreply.github.com>
2017-12-05 16:06:15 +0900

    Create 3.R

---
+++ 3.R
@@ -0,0 +1,8 @@
+df <- read.table("data.csv",header = 1,sep = ",")
+df <- df[complete.cases(df),]
+tmp_time <- paste(paste(df$year, df$month, df$day, sep="-"),  
+              paste(df$hour, 00, 00, sep=":"), sep = " ") 
+tmp_time <- as.POSIXct(tmp_time)
+df$timestamp <- tmp_time
+df <- df[,c(1,2,6:14)]
+pairs(df[c("pm2.5","DEWP","TEMP")])
