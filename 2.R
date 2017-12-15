7b30c57f0a3edb8d2ca5ba622a71304e49cede54
taka0409 <32897506+taka0409@users.noreply.github.com>
2017-12-15 16:06:12 +0900

    Update 2.R

--- 2.R
+++ 2.R
@@ -2,6 +2,7 @@
 df <- read.table("data.csv",header = 1,sep = ",")
 df <- df[complete.cases(df),]
 df <- df[,c(6:9,11:13)]
+par(mfrow=c(2,4))
 for(i in 1:ncol(df)){
   hist(df[,i],xlab=colnames(df[i]),main=colnames(df[i]))
 }
