004d7dd02fae7c9ccaa03f622db8ae695fd42032
taka0409 <32897506+taka0409@users.noreply.github.com>
2017-12-15 16:00:55 +0900

    Update 2.R

--- 2.R
+++ 2.R
@@ -1,12 +1,7 @@
+
 df <- read.table("data.csv",header = 1,sep = ",")
 df <- df[complete.cases(df),]
-tmp_time <- paste(paste(df$year, df$month, df$day, sep="-"),  
-              paste(df$hour, 00, 00, sep=":"), sep = " ") 
-tmp_time <- as.POSIXct(tmp_time)
-df$timestamp <- tmp_time
-df <- df[,c(1,2,6:9,11:14)]
-par(mfrow=c(2,4))
-max(df$Iws)
-for(i in 3:9){
-	hist(df[,i],xlab=colnames(df[i]),main=colnames(df[i]))
+df <- df[,c(6:9,11:13)]
+for(i in 1:ncol(df)){
+  hist(df[,i],xlab=colnames(df[i]),main=colnames(df[i]))
 }
