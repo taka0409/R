e142f2e5f6dc2a178f2025c391d4d87f345e9fcf
Taka <takatowin0510@gmail.com>
2017-12-16 16:00:41 +0900

    up

--- 2.R
+++ 2.R
@@ -6,7 +6,6 @@ tmp_time <- as.POSIXct(tmp_time)
 df$timestamp <- tmp_time
 df <- df[,c(1,2,6:9,11:14)]
 par(mfrow=c(2,4))
-max(df$Iws)
 for(i in 3:9){
 	hist(df[,i],xlab=colnames(df[i]),main=colnames(df[i]))
 }
